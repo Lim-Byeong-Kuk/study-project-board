d722e5f0ae9524975f2bd7ac5264ecbf26564048
Lim-Byeong-Kuk <bye0414@naver.com>
2024-03-13 13:23:42 +0900

    #1 - Spring Data REST 테스트 제외 처리
    이것은 이 프로젝트의 비지니스 로직으로 구현한 내용이 아니라 data rest 기능이고,
    통합테스트라 무거우며 db 에도 영향을 준다.
    공부 목적으로도 의미는 있기 때문에 삭제하지는 않고 제외 처리해서 전체 테스트 중에 실행 되지 않게끔 처리

--- src/test/java/com/example/projectboard/controller/DataRestTest.java
+++ src/test/java/com/example/projectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.example.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+@Disabled("Spring Data Rest 통합테스트느는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc   // SpringBootTest 는 MockMvc 의 존재를 모름
