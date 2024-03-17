65a83e0cdae414b28a66f2546e63be2e421d4aa1
Lim-Byeong-Kuk <bye0414@naver.com>
2024-03-17 14:51:01 +0900

    #16 - thymeleaft3 decoupled logic 설정
    java code 로 thymeleaf3 decoupled logic 을
    사용자 application property 로 제어할 수 있게
    설정 추가
    
    https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

---
+++ src/main/java/com/example/projectboard/config/ThymeleafConfig.java
@@ -0,0 +1,40 @@
+package com.example.projectboard.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+/**
+ *  configuration property 를 유저가 직접 만들 경우
+ *  반드시 스캔을 해줘야 한다.
+ *  main 클래스에서 @ConfigurationPropertiesScan 어노테이션을 달아줘야 함
+ */
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+
+        return defaultTemplateResolver;
+    }
+
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding
+    @ConfigurationProperties("spring.thymeleaf3")
+    public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
+        private final boolean decoupledLogic;
+    }
+}
