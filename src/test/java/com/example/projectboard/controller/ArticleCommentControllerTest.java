332807c7f8814d3f6dcd1cfcdfb226be5096d296
Lim-Byeong-Kuk <bye0414@naver.com>
2024-05-08 13:26:01 +0900

    #72 - 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가
    대댓글이 포함된 댓글 저장 api 의 입출력 테스트 추가

--- src/test/java/com/example/projectboard/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleCommentControllerTest.java
@@ -7,6 +7,8 @@ import com.example.projectboard.service.ArticleCommentService;
 import com.example.projectboard.util.FormDataEncoder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -88,6 +90,28 @@ class ArticleCommentControllerTest {
                 .andExpect(view().name("redirect:/articles/" + articleId))
                 .andExpect(redirectedUrl("/articles/" + articleId));
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
+    }
+
+    @WithUserDetails(value = "lbkTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
+    @Test
+    void givenArticleCommentInFoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
+        // given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+        BDDMockito.willDoNothing().given(articleCommentService).saveArticleComment(ArgumentMatchers.any(ArticleCommentDto.class));
+
+        // when & then
+        mvc.perform(
+                post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf())
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+        BDDMockito.then(articleCommentService).should().saveArticleComment(ArgumentMatchers.any(ArticleCommentDto.class));
     }
 
 }
