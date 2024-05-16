a886b4c2e8380659c39713f43ef97b6dbc5e1d62
Lim-Byeong-Kuk <bye0414@naver.com>
2024-05-16 14:45:52 +0900

    #83 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/example/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/example/projectboard/config/TestSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.projectboard.config;
 
-import com.example.projectboard.domain.UserAccount;
-import com.example.projectboard.repository.UserAccountRepository;
+import com.example.projectboard.dto.UserAccountDto;
+import com.example.projectboard.service.UserAccountService;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,21 +10,32 @@ import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
     @MockBean
-    private UserAccountRepository userAccountRepository;
+    private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        BDDMockito.given(userAccountRepository.findById(ArgumentMatchers.anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "lbkTest",
                 "pw",
                 "lbk-test@email.com",
                 "lbk-text",
                 "test memo"
-        )));
+        );
     }
 
 }
