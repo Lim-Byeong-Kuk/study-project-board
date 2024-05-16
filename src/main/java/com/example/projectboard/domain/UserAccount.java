af8d2c2bb57ae7c486905ddeb6ba5cd5d3add820
Lim-Byeong-Kuk <bye0414@naver.com>
2024-05-16 14:41:16 +0900

    #83 - 회원 엔티티 디자인 수정: 인증이 없는 상태에서 회원 정보를 DB에 등록하기 위한 생성자 메서드 추가
    인증 정보가 없을 땐 직접 생성자, 수정자 정보를 입력하여
    회원 등록을 할 수 있도록 생성자 메서드를 추가함

--- src/main/java/com/example/projectboard/domain/UserAccount.java
+++ src/main/java/com/example/projectboard/domain/UserAccount.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
@@ -37,16 +37,22 @@ public class UserAccount extends AuditingFields {
 
     public UserAccount() {}
 
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;    // 생성 시점에는 수정자와 생성자가 같다.
     }
 
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
